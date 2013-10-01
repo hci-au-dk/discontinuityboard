c37ad6b0c2512bbd96f3a30be286f6e18eb2f0c4
Grace Muzny <muznyg@gmail.com>
2013-10-01 12:25:14 +0200

    imposing a basic structure on the app environment

--- discontinuityboard.py
+++ discontinuityboard.py
@@ -1,10 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
+from app import app
 
-@app.route("/")
-def hello():
-    return "Hello, world!"
 
-if __name__ == "__main__":
-    app.run()
+app.run()
     
