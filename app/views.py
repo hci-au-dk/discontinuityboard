271d084c72c82a38fd968973cb6fdb5159728a0d
Grace Muzny <muznyg@gmail.com>
2013-10-01 13:58:04 +0200

    Added some basic templates for the app

--- app/views.py
+++ app/views.py
@@ -2,5 +2,7 @@ from app import app
 from flask import render_template
 
 @app.route("/")
-def hello():
-    return "Hello, world!"
+@app.route("/index")
+def index():
+    return render_template("index.html",
+                           title = "Discontinuity Board")
