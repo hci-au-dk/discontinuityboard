bbc078b171711cc281f2500127ac78981d32de29
Grace Muzny <muznyg@gmail.com>
2013-10-02 11:22:13 +0200

    basic file upload and display

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,9 @@
+from flask.ext.uploads import UploadSet, IMAGES
 from flask.ext.wtf import Form
 from werkzeug import secure_filename
-from flask_wtf.file import FileField
-from wtforms import TextField
-from wtforms.validators import Required
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+
+images = UploadSet('images', IMAGES)
 
 class PhotoUploadForm(Form):
-    photo = FileField('Your Photo', validators = [Required()])
+    photo = FileField('Your Photo')
--- app/views.py
+++ app/views.py
@@ -1,22 +1,40 @@
+import os, tempfile
 from app import app
-from flask import render_template, flash, redirect
-from forms import PhotoUploadForm
+from flask import render_template, flash, redirect, request, url_for, send_from_directory
 from werkzeug import secure_filename
 
-@app.route("/")
+UPLOAD_FOLDER = tempfile.gettempdir()
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file', filename=filename))
+    return render_template('index.html')
+
+
 @app.route("/index")
 def index():
-    print "hi"
-    return render_template("index.html",  # if you change the title, change the test!
-                           title = "Discontinuity Board")  
+    return render_template('index.html',  # if you change the title, change the test!
+                           title = 'Discontinuity Board')  
 
-@app.route('/upload', methods = ('GET', 'POST'))
-def upload():
-    form = PhotoUploadForm()
-    if form.validate_on_submit():
-#        filename = secure_filename(form.photo.data.filename)
-        flash('Uploaded file: ' + form.photo.data)
-        return redirect('/index')
+@app.route('/show/<filename>')
+def uploaded_file(filename):
+    filename = 'http://127.0.0.1:5000/uploads/' + filename
+    return render_template('show.html', filename=filename)
 
-    return render_template('upload.html',
-                           form = form)
+@app.route('/uploads/<filename>')
+def send_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+
